5eb3aa914c689c2ecefb39bc42324e22b80a9b0a
BoBo <BoBo@DTC>
2014-06-24 12:52:35 +0200

    - directive auto enter key press

--- js/directives.js
+++ js/directives.js
@@ -307,3 +307,17 @@ FeaderAppDirectives.directive('link', [function() {
         };
     }
 ]);
+
+FeaderAppDirectives.directive('ngEnter', [function() {
+        return function(scope, element, attrs) {
+            element().bind('keydown keypress', function(e) {
+                if (e.which === 13) {
+                    scope.$apply(function() {
+                        scope.$eval(attrs.ngEnter, {'event':e});
+                    });
+                    e.preventDefault();
+                }
+            });
+        };
+    }
+]);

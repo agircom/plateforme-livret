f34a09cdace7463e0fca0e385dce883afe11e3af
BoBo <BoBo@DTC>
2014-07-12 16:08:42 +0200

    - fix faq newline answer

--- js/filters.js
+++ js/filters.js
@@ -29,3 +29,10 @@ FeaderAppFilters.filter('startFrom', [function() {
         };
     }
 ]);
+
+FeaderAppFilters.filter('breakFilter', [function() {
+        return function(text) {
+            if (text !== undefined) return text.replace(/\n/g, '<br />');
+        };
+    }
+]);

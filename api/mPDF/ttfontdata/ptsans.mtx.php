f7e5ed0961fba761f7792c9c1e4b659b7e5074fb
Vibration76 <shapple76000@gmail.com>
2014-07-29 12:09:22 +0200

    modifs

---
+++ api/mPDF/ttfontdata/ptsans.mtx.php
@@ -0,0 +1,26 @@
+<?php
+$name='PTSans-Regular';
+$type='TTF';
+$desc=array (
+  'Ascent' => 1018,
+  'Descent' => -276,
+  'CapHeight' => 700,
+  'Flags' => 4,
+  'FontBBox' => '[-355 -245 1093 993]',
+  'ItalicAngle' => 0,
+  'StemV' => 87,
+  'MissingWidth' => 750,
+);
+$up=-75;
+$ut=50;
+$ttffile='C:/wamp/www/plateforme-livret/api/mPDF/ttfonts/PT_Sans-Web-Regular.ttf';
+$TTCfontID='0';
+$originalsize=442960;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='ptsans';
+$panose=' 8 2 2 b 5 3 2 2 3 2 2 4';
+$haskerninfo=false;
+$unAGlyphs=false;
+?>

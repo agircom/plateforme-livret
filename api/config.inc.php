80c91a4ec123bca525a186b949b151e6e548d4c4
Kevin <kcauchois@ftel.fr>
2015-01-30 17:01:28 +0100

    corrections diverses

--- api/config.inc.php
+++ api/config.inc.php
@@ -1,1 +1,26 @@
+<?php
 
+return array(
+    'app' => array(
+        'salt' => '!P10p&42!'
+    ),
+    'bdd' => array(
+
+        'host' => 'localhost',
+        'name' => 'livretac',
+        'user' => 'root',
+        'passwd' => ''
+    ),
+    'smtp' => array(
+//        'host' => 'SSL0.OVH.NET',
+        'host' => 'smtp.livret-accueil-haute-normandie.fr',
+        'port' => 587,
+        'auth' => true,
+        'user' => 'plateforme@livret-accueil-haute-normandie.fr',
+        'passwd' => '4x8Fvm4J',
+        'secure' => 'ssl',
+        'debug' => 0,
+        'from' => 'plateforme@livret-accueil-haute-normandie.fr',
+        'from_name' => 'Plateforme livret d\'accueil'
+    )
+);
